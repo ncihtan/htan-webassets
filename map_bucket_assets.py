79989362f73d2ad9e9be8e3d78b48db484a93c32
Jason Hwee <1216418+hweej@users.noreply.github.com>
2021-09-21 17:25:08 -0400

    Convert to new htan-assets bucket. Add option to supply cdn domain name from CLI.

--- map_bucket_assets.py
+++ map_bucket_assets.py
@@ -17,9 +17,9 @@ from collections import defaultdict
 def process_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-b', '--bucket', type=str, required=True, help='Bucket name')
-    parser.add_argument('-s', '--schematic-bucket-manifest', help='A schematic manifest file with relevant metadata')
     parser.add_argument('-m', '--bucket-manifest', help='')
     parser.add_argument('-a', '--assets', help='')
+    parser.add_argument('--cdn', required=True, help='Domain name for AWS Cloudfront CDN')
     args = parser.parse_args()
     return args
 
@@ -29,13 +29,11 @@ def main():
     bucket_manifest = args.bucket_manifest
     bucket = args.bucket
     assets = args.assets
+    domain_name = args.cdn
 
-    # S3_ASSETS_BUCKET = 'https://htan-imaging-example-datasets.s3.amazonaws.com'
-    # S3_ASSETS_URL_FORMAT = '{S3_ASSETS_BUCKET}/{S3_KEY}'
-    S3_ASSETS_URL_FORMAT = 'https://htan-imaging-example-datasets.s3.amazonaws.com/{S3_KEY}'
+    S3_ASSETS_URL_FORMAT = 'https://%s/{S3_KEY}' % domain_name
 
     # Parse bucket_manifest
-    manifest_headers = ['bucket_name', 'prefix']
     keys = []
     with open(bucket_manifest, 'r') as fh:
         for line in fh:
@@ -46,19 +44,18 @@ def main():
     thumbnails = defaultdict(str)
     m_stories = defaultdict(list)
 
-    thumbnail_loc = f'miniature-drafts/{bucket_name}/'
-    m_stories_gsloc = f'auto-minerva-drafts/gs/{bucket_name}/'
-    m_stories_s3loc = f'auto-minerva-drafts/s3/{bucket_name}/'
+    thumbnails_prefix = f'thumbnails/{bucket_name}/'
+    minerva_stories_prefix = f'minerva_stories/{bucket_name}/'
     with open(assets, 'r') as fh:
         for line in fh:
             bucket_name, s3key = line.strip().split('\t')
-            if s3key.startswith(thumbnail_loc):
+            if s3key.startswith(thumbnails_prefix):
                 thumbnails['/'.join(s3key.split('/')[2:])] = s3key
-            elif s3key.startswith(m_stories_s3loc) or s3key.startswith(m_stories_gsloc):
+            elif s3key.startswith(minerva_stories_prefix):
                 if 'Group' in s3key:
                     pass
                 else:
-                    asset_lookup_name = '/'.join(s3key.split('/')[3:-1])  # indexOf "imaging_level_2"
+                    asset_lookup_name = '/'.join(s3key.split('/')[2:-1])  # indexOf "imaging_level_2"
                     m_stories[asset_lookup_name].append(s3key)
 
     # for k, v in m_stories.items():
@@ -92,30 +89,6 @@ def main():
             records.append(rec)
 
     print(json.dumps(records))
-
-    # Map bucket manifest objects with available thumbnails
-    """
-    [
-      {
-        "htan_center": "HTAN HMS",
-        "filename": "imaging_level_2/CRC202105/CRC17.ome.tif",
-        "htan_parent_biospecimen_id": "HTA7_982_2",
-        "htan_data_file_id": "HTA7_982_1000",
-        "imaging_assay_type": "t-CyCIF",
-        "size_c": 36,
-        "thumbnail": "CRC17.ome.png"
-      },
-      {
-        "htan_center": "HTAN HMS",
-        "filename": "imaging_level_2/CRC202105/CRC09.ome.tif",
-        "htan_parent_biospecimen_id": "HTA7_989_2",
-        "htan_data_file_id": "HTA7_989_1000",
-        "imaging_assay_type": "t-CyCIF",
-        "size_c": 36,
-        "thumbnail": "CRC09.ome.png"
-      }
-    ]
-    """
 
 
 if __name__ == '__main__':
